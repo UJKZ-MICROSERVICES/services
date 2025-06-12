033c1e48ad9f5122e69f0ec5b7dee9c5cd44228c
Yougsim <52629367+Yougsim@users.noreply.github.com>
2025-06-12 19:02:54 +0000

    Delete student-service directory

--- student-service/src/main/java/com/student/service/repositories/FiliaireRepository.java
+++
@@ -1,9 +0,0 @@
-package com.student.service.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.student.service.models.Filiaire;
-
-public interface FiliaireRepository extends JpaRepository<Filiaire, Integer>{
-
-}
